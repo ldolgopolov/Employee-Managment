85d4cf74af302577d88272131f18faa73facb35e
LeonNapoleonov <142759206+ldolgopolov@users.noreply.github.com>
2024-09-15 15:20:58 +0300

    Created a Spring Boot project

---
+++ back-employee/src/main/java/com/example/back_employee/BackEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEmployeeApplication.class, args);
+	}
+
+}
---
+++ back-employee/src/test/java/com/example/back_employee/BackEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
